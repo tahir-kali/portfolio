3005757f4f062c9e1c35b53b4b8c301bdee06ce7
Codeholic <irenic.tahir@gmail.com>
2023-05-29 16:29:05 +0500

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,59 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header unelevated class="bg-white">
+
+      <q-img to="/" src="letter-t.gif"  class="logo fixed-top-left" />
+
+      <q-toolbar>
+        <div class="text-black flex full-width justify-center text-h6">
+          <q-btn flat no-caps v-for="link in links" :key="'Link'+link.label" class="link" :to="link.link">
+            {{  link.label }}
+          </q-btn>
+        </div>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+<script setup>
+const links = [
+  {
+    label: "About",
+    link: "/about"
+  },
+  {
+    label: "Experience",
+    link: "/experience"
+  },
+  {
+    label: "Work",
+    link: "/work"
+  },{
+    label: "Contact",
+    link: "/contact"
+  }
+];
+</script>
+<style>
+.logo{
+  cursor: pointer;
+  margin: 5px;
+  border-radius: 10px;
+  padding: 2px;
+  width: 50px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.main-container{
+  width: 100%;
+  min-height:100%;
+}
+.link{
+  border-bottom: 3px solid white;
+}
+.link:hover{
+  border-bottom: 3px solid #00cfb4;
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="main-container flex">
+  <!-- <q-img src="guard.gif" fit="contain" class="guard" /> -->
+</div>
+
+</template>
+
+<style scoped>
+.guard{
+  width: min(500px,99%);
+  margin-inline: auto;
+  margin-block: 5%;
+}
+</style>
